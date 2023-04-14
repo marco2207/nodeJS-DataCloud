3111c6f93f7b07bb9abbaadea5ce8513698db5b7
marco2207 <drake2207@gmail.com>
2023-04-14 10:20:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const http = require('http');
-const express = require('express');
+
+const app = require('express');
 const path = require('path');
-const app = express();
+const PORT = process.env.PORT || 5000
+const http = require('https');
+
 app.use(express.json());
 app.use(express.static("express"));
+
 // default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
   });
-const server = http.createServer(app);
-const PORT = process.env.PORT || 5000
+const server = https.createServer(app);
+
 server.listen(PORT);
 console.debug('Server listening on port ' + PORT);
