52a2a74a067d91749b3778116e7180e8dd7479a0
marco2207 <drake2207@gmail.com>
2023-05-19 11:38:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/addpg', function(req,res){
-  console.log('@@@ data sent by form are: ' + req.body.Name + ", " + req.body.Email + ", " + req.body.Message);
+  console.log('@@@ Data sent by form are: ' + req.body.Name + ", " + req.body.Email + ", " + req.body.Message);
   var query = "INSERT INTO salesforce.Richieste__c (email__c, Name__c, Messaggio__c) VALUES (" + "'" + req.body.Email + "'" + ", " + "'" + req.body.Name + "'" + ", " + "'" + req.body.Message + "'" + ")";
-  client.query(query, function(err){
-     console.log("@@@ Wrote on Postgres");
-     console.log("@@@" + err);
-   });
-  console.log("@@@ New message has been added");
-  res.redirect('/');
+  console.log("@@@ Query string: " + query);
+  client.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('@@@ Message added');
+    client.end();
+  });
+res.redirect('/');
 });
  
 // Start the server
