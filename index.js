dd6568b3bc3391fdeeda745b0a45db2461242a1c
marco2207 <drake2207@gmail.com>
2023-05-19 09:40:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const pg = require("pg");
+
+/*
+* PG Client connection
+*/
+pg.defaults.ssl = true;
+var dbString = process.env.DATABASE_URL;
+var sharedPgClient;
+var client = new pg.Client(dbString);
+client.connect();
  
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/express/index.html'));
+});
+
+app.post('/addpg', function(req,res){
+  console.log('@@@ data sent by form are: ' + req.Name + ", " + req.Email + ", " + req.Message);
+  var query = "INSERT INTO salesforce.Richieste__c (email__c, name__c, message__c) VALUES (" + "'" + req.Email + "'" + ", " + "'" + req.Name + "'" + ", " + "'" + req.Message + "'")";
 });
  
 // Start the server
