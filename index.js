e895c8e8b34c81df61ccccf85789f7498e94a9ef
marco2207 <drake2207@gmail.com>
2023-05-19 10:22:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const pg = require("pg");
+var bodyParser = require('body-parser');
 
 /*
 * PG Client connection
@@ -12,13 +13,15 @@ var dbString = process.env.DATABASE_URL;
 var sharedPgClient;
 var client = new pg.Client(dbString);
 client.connect();
+
+app.use(bodyParser.urlencoded({extended: false}));
  
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/express/index.html'));
 });
 
 app.post('/addpg', function(req,res){
-  //console.log('@@@ data sent by form are: ' + req.body.Name + ", " + req.body.Email + ", " + req.body.Message);
+  console.log('@@@ data sent by form are: ' + req.body.Name + ", " + req.body.Email + ", " + req.body.Message);
   //var query = "INSERT INTO salesforce.Richieste__c (email__c, name__c, message__c) VALUES (" + "'" + req.body.Email + "'" + ", " + "'" + req.body.Name + "'" + ", " + "'" + req.body.Message + "'" + ")";
   console.log("@@@ New message has been added");
   res.send("New employee has been added into the database");
