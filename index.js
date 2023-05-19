24537a248fc242862a67b8ef742d77441be51674
marco2207 <drake2207@gmail.com>
2023-05-19 11:51:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ var bodyParser = require('body-parser');
 pg.defaults.ssl = true;
 var dbString = process.env.DATABASE_URL;
 var sharedPgClient;
-var client = new pg.Client(dbString);
+var client = new pg.Client({
+  connectionString: connectionString,
+  ssl: { rejectUnauthorized: false }
+})
 client.connect();
 
 app.use(bodyParser.urlencoded({extended: false}));
